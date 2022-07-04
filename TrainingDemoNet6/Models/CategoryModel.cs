0c2211805853a65acc0ef2f4a8385f0659d69d48
James Shaun <SHAUNJ@bridgwater.ac.uk>
2022-07-04 16:48:31 +0100

    Fixed issue where toastr wasnt showing on create and edit

--- TrainingDemoNet6/Models/CategoryModel.cs
+++ TrainingDemoNet6/Models/CategoryModel.cs
@@ -8,7 +8,7 @@ namespace TrainingDemoNet6.Models
         [Key]
         public int Id { get;set; }
         [Required]
-        public string Name { get;set; }
+        public string? Name { get;set; }
 
         [Required]
         [DisplayName("Display Order")]
